5e4b0920a91c6e80a8fca840b64c410433fb291a
chao lobi <gogoiladitya@gmail.com>
2023-05-08 11:37:54 +0530

    services section is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,12 +41,14 @@ const skillsHeader = document.querySelectorAll('.skills__header');
 function toggleSkills() {
     let itemClass = this.parentNode.className;
 
-    for (i = 0; i < skillsContent.length; i++) {
+    for (let i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = 'skills__content skills__close'
     }
 
     if (itemClass == 'skills__content skills__close') {
-        this.parentNode.className = 'skills_content skills__open';
+        this.parentNode.className = 'skills__content skills__open';
+    } else {
+        this.parentNode.className = 'skills__content skills__close';
     }
 }
 
@@ -57,10 +59,50 @@ skillsHeader.forEach((el) => {
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        })
+
+        target.classList.add('qualification__active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualificatiion__active');
+        })
+
+        tab.classList.add('qualification__active');
+    })
+})
 
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalClose = document.querySelectorAll('.services__modal-close');
 
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    })
+})
+
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 

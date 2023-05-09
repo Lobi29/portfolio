249447ddeb06c0ba91a05241f76c3b677b6e1087
chao lobi <gogoiladitya@gmail.com>
2023-05-09 11:20:00 +0530

    portfolio section is partially done

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,7 +105,19 @@ modalClose.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+        nextE1: '.swiper-button-next',
+        prevE1: '.swiper-button-prev',
+    },
+    pagination: {
+        e1: '.swiper-pagination',
+        clickable: true
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
